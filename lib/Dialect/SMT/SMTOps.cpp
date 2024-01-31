84f4a821aba25ded605cd90e9dafa0c644e870b4
Martin Erhart <maerhart@outlook.com>
2024-01-31 14:14:57 +0100

    [SMT] Add integer constant and arithmetic operations

--- lib/Dialect/SMT/SMTOps.cpp
+++ lib/Dialect/SMT/SMTOps.cpp
@@ -9,6 +9,7 @@
 #include "circt/Dialect/SMT/SMTOps.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/OpImplementation.h"
+#include "llvm/ADT/APSInt.h"
 
 using namespace circt;
 using namespace smt;
@@ -65,6 +66,44 @@ void BoolConstantOp::getAsmResultNames(
 OpFoldResult BoolConstantOp::fold(FoldAdaptor adaptor) {
   assert(adaptor.getOperands().empty() && "constant has no operands");
   return getValueAttr();
+}
+
+//===----------------------------------------------------------------------===//
+// IntConstantOp
+//===----------------------------------------------------------------------===//
+
+void IntConstantOp::getAsmResultNames(
+    function_ref<void(Value, StringRef)> setNameFn) {
+  SmallVector<char, 32> specialNameBuffer;
+  llvm::raw_svector_ostream specialName(specialNameBuffer);
+  specialName << "c" << getValue();
+  setNameFn(getResult(), specialName.str());
+}
+
+OpFoldResult IntConstantOp::fold(FoldAdaptor adaptor) {
+  assert(adaptor.getOperands().empty() && "constant has no operands");
+  return getValueAttr();
+}
+
+void IntConstantOp::print(OpAsmPrinter &p) {
+  p << " " << getValue();
+  p.printOptionalAttrDict((*this)->getAttrs(), /*elidedAttrs=*/{"value"});
+}
+
+ParseResult IntConstantOp::parse(OpAsmParser &parser, OperationState &result) {
+  APInt value;
+  auto res = parser.parseOptionalInteger(value);
+  if (!res.has_value())
+    return failure();
+
+  result.addAttribute("value",
+                      IntegerAttr::get(parser.getContext(), APSInt(value)));
+
+  if (parser.parseOptionalAttrDict(result.attributes))
+    return failure();
+
+  result.addTypes(smt::IntegerType::get(parser.getContext()));
+  return success();
 }
 
 //===----------------------------------------------------------------------===//
